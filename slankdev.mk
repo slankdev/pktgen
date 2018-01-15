bc43bbe758e2fcbf166c4cd17319f59c3eee8b86
Hiroki SHIROKURA <slank.dev@gmail.com>
2018-01-15 09:59:35 +0900

    2018.01.15-09:59

--- slankdev.mk
+++ slankdev.mk
@@ -6,11 +6,23 @@ bind2:
 	sudo dpdk_nic_bind -b uio_pci_generic 01:00.1
 	sudo dpdk_nic_bind -b uio_pci_generic 01:00.0
 
-run:
+run2:
 	sudo $(PGM) \
 		-- \
 		-P \
-		-m "[1:2-3].0,[5:6-7].1"
+		-m "[1-4].0,[16-19].1" -f range.conf
+
+run4:
+	sudo $(PGM) \
+		-- \
+		-P \
+		-m "[1-4].0,[16-19].1,[8-11].2,[24-29].3" -f range.conf
+
+script:
+	sudo $(PGM) \
+		-- \
+		-P \
+		-m "[1-4].0,[16-19].1,[8-11].2,[24-27].3" -f traf.lua
 
 kill:
 	sudo killall $(PROG_NAME)
